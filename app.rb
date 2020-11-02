c8e89f7c6c1040c1b8db7ee66403dedc39be8b33
Sara A <calisaraalhindi@gmail.com>
2020-11-02 23:19:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,11 +6,27 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
+ 
     erb :reversed
   end
 
+ 
+  
+  # get "/" do
+  #   @user = "Ian"
+ 
+  #   erb :index # @user will be defined as 'Ian' in the view
+  # end
+ 
+  # get "/profile" do
+  #   erb :profile # @user will be nil here
+  # end
+  
   get '/friends' do
-    # Write your code here!
-
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+ 
+    erb :friends
   end
 end
